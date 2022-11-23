34f4c6b930d2db32ee1b06c80aa70d984ed59b07
hogkim <hogkim@c4r10s1.42seoul.kr>
2022-11-23 14:46:45 +0900

    cpp05

--- CPP_Module_05/ex00/Bureaucrat.cpp
+++ CPP_Module_05/ex00/Bureaucrat.cpp
@@ -6,9 +6,21 @@
 /*   By: hogkim <hogkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:46:48 by hogkim            #+#    #+#             */
-/*   Updated: 2022/11/22 15:47:50 by hogkim           ###   ########.fr       */
+/*   Updated: 2022/11/23 14:46:38 by hogkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include <iostream>
 
+Bureaucrat::Bureaucrat()
+	: _name("nameless"), _grade(1)
+{
+	std::cout << "Default constructor of Bureaucrat has been called" << std::endl;
+};
+
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade)
+	: _name(name), _grade(grade);
+{
+	std::cout << "Constructor of Bureaucrat has been called" << std::endl;
+};
--- CPP_Module_05/ex00/Bureaucrat.hpp
+++ CPP_Module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: hogkim <hogkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:46:51 by hogkim            #+#    #+#             */
-/*   Updated: 2022/11/22 15:55:44 by hogkim           ###   ########.fr       */
+/*   Updated: 2022/11/23 14:42:46 by hogkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,24 @@ class Bureaucrat
 {
 private:
 	const std::string	_name;
-	size_t				_grade;
+	unsigned int		_grade;
 public:
+	Bureaucrat();
+	Bureaucrat(std::string name, unsigned int grade);
+	Bureaucrat(const Bureaucrat& obj);
+	~Bureaucrat();
+	Bureaucrat& operator=(const Bureaucrat &obj);
+	
+	class GradeTooHighException : public std::exception
+	{};
+	
+	class GradeTooLowException : public std::exception
+	{};
 
+	std::string	getName() const;
+	int			getGrade() const;		
+	void		incrementGrade();
+	void		decrementGrade();	
 };
 
 
